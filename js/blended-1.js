ee0d778c32ce37501901fb53805c2bcdea073947
LudSkop <luda.skopenko80@gmail.com>
2025-12-10 23:30:35 +0200

    solve task  13

--- js/blended-1.js
+++ js/blended-1.js
@@ -249,3 +249,23 @@ function createReversedArray() {
 console.log(createReversedArray(12, 85, 37, 4));
 console.log(createReversedArray(1, 2, 3, 4));
 
+// № 13. Масив colors містить колекцію кольорів. Кожен колір представлений об'єктом і має властивості hex і rgb з відповідними для цього формату і кольору значеннями.
+
+//Перебери масив об'єктів colors, використовуючи цикл for...of. Додай у масив hexColors значення властивостей hex, а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+for ( const key of colors){
+  hexColors.push(key.hex);
+  rgbColors.push(key.rgb)
+  
+}
+console.log("hexColors:", hexColors);
+console.log("rgbColors:", rgbColors);
